83e28c217417f9c0f2383478525b80e49b2a7b7d
Alfredo <alfredocortez12god@gmail.com>
2024-10-20 23:43:51 -0400

    galeria.php

--- galeria.php
+++ galeria.php
@@ -19,7 +19,7 @@
 <nav>
     <ul>
         <li><a href="index.php">Inicio</a></li>
-        <li><a href="galerias.php">Galería</a></li>
+        <li><a href="galeria.php">Galería</a></li>
         <li><a href="#">Tatuadores</a></li>
         <li><a href="#">Servicios</a></li>
         <li><a href="contacto.php">Contacto</a></li>
